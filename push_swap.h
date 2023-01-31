457f33c5b5919bb752bde402d40438df2c0753bd
Nathan Vaubien <nvaubien@c2r6s1.42lausanne.ch>
2023-01-31 19:20:38 +0100

    Makefile fix

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nvaubien <nvaubien@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 16:22:04 by nvaubien          #+#    #+#             */
-/*   Updated: 2023/01/31 17:03:25 by nvaubien         ###   ########.fr       */
+/*   Updated: 2023/01/31 18:41:26 by nvaubien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,15 @@
 #include "libs/libft/libft.h"
 #include "libs/ftprintf/ft_printf.h"
 
+/* Structs */
+typedef struct s_node {
+	int data;
+	struct s_node *next;
+} t_node;
+
 /* Fonctions */
 int main();
+
 
 #endif
 
